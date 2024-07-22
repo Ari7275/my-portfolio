ab4c1b807f890592a38d224327bff370fe398d5c
Ari7275 <rywyyn@gmail.com>
2024-07-22 18:55:57 +0300

    update

--- app/components/NavBar.tsx
+++ app/components/NavBar.tsx
@@ -1,5 +1,6 @@
 "use client"
 
+import Image from "next/image";
 import Link from "next/link";
 
 export default function NavBar() {
@@ -17,14 +18,22 @@ export default function NavBar() {
     }
 
     return (
-        <div className="fixed top-[0px] p-5 z-40 bg-white h-[10%]  w-full">
-            <div className="">
+        <div className="fixed top-0 p-5 z-40 bg-white h-[10%] w-full">
+            <div className="flex justify-between items-center">
                 <div className="flex flex-row text-lg font-bold gap-8">
                     <button onClick={() => scrollToSection('home')}>Home</button>
                     <button onClick={() => scrollToSection('about-me')}>About Me</button>
                     <button onClick={() => scrollToSection('projects')}>Projects</button>
                     <button onClick={() => scrollToSection('contact-me')}>Contact Me</button>
                 </div>
+
+                <Image
+                className="mr-14"
+                    width={50}
+                    height={50}
+                    alt=""
+                    src={"/icons/navbar.png"}
+                />
             </div>
         </div>
     );
--- app/components/SideButtons.tsx
+++ app/components/SideButtons.tsx
@@ -5,9 +5,9 @@ import { BiLogoInstagram, BiLogoLinkedin } from "react-icons/bi";
 export default function SideButtons() {
     return (
         <div className="side-buttons">
-            <a href="https://github.com/Ari7275"><FiGithub/></a>
-            <a href="https://www.linkedin.com/in/ari-vainer/"><BiLogoLinkedin/></a>
-            <a href="https://www.instagram.com/ari.vainer/"><BiLogoInstagram/></a>
+            <a target="_blank" href="https://github.com/Ari7275"><FiGithub/></a>
+            <a target="_blank" href="https://www.linkedin.com/in/ari-vainer/"><BiLogoLinkedin/></a>
+            <a target="_blank" href="https://www.instagram.com/ari.vainer/"><BiLogoInstagram/></a>
         </div>
     )
 }
