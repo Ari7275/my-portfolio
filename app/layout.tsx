6df02555af5996722990195892dbb4b697d57d12
Ari7275 <rywyyn@gmail.com>
2024-08-20 19:27:28 +0300

    few changs
    hi

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,7 +5,7 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Ari vainer",
+  title: "Portfolio",
   description: "this is my portfolio website!",
 };
 
