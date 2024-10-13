16b495db31d9c9675fed8f4a9de112202f14d236
Ari7275 <rywyyn@gmail.com>
2024-10-13 10:53:04 +0300

    fix

--- app/page.tsx
+++ app/page.tsx
@@ -5,14 +5,20 @@ import NavBar from "./components/NavBar";
 import Body from "./components/Body";
 import SideButtons from "./components/SideButtons";
 import { ArrowUpIcon } from "@heroicons/react/16/solid";
+import { useState } from "react";
+import useMediaQuery from "./hooks/useMediaQuery";
 
 export default function Home() {
+
+  const isAboveMediumScreens = useMediaQuery("(min-width: 1060px)");
+
+
 
 
   return (
 
     <div className="h-full w-full dark:bg-[#2C2C2C]">
-      <main className="relative h-full w-full p-6 ">
+      <main className={`relative h-full w-full ${isAboveMediumScreens ? "" : "p-6"}`}>
         <NavBar></NavBar>
         <SideButtons/>
         <Body></Body>
